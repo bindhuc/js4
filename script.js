231c93e7b60e67d2ab3cd60b0a8e8f3f8b962bf4
bindhuc <bindhusai002@gmail.com>
2022-03-23 01:41:43 -0700

    first

---
+++ script.js
@@ -0,0 +1,81 @@
+var a=10;
+
+if(a=="10")
+console.log(a +"is 10")
+
+// if else
+
+// if(condition){
+//     // true condition
+// }
+// else{
+//     //false condition
+// }
+
+var password="aaaa"
+if(password=="aaaa"){
+    console.log("correct password")
+}
+else{
+    console.log("incorrect password")
+}
+
+var number=6;
+if(number%2==6){
+    console.log(number +"is even")
+}
+else{
+    console.log(number + "is odd")
+}
+
+var abc=10;
+if(condition1)
+{
+console.log("a")
+
+
+}
+
+
+var marks=35;
+if((marks>=0)&&marks(marks<30)){
+    console.log("fail")
+}
+else if((marks>=30)&&(marks<50))
+{
+    console.log("A grade")
+}
+else if ((marks>100)&&(marks<=10)) 
+{
+console.log("c")
+}
+else{
+    console.log("invalid")
+}
+
+
+// switch
+
+var button=1;
+switch (button){
+    case "1"  :
+        return button
+        case "2" :
+            return button
+            case "3" :
+                return button
+                default :
+                return button
+}
+
+var button=1;
+switch (button){
+    case "1"  :
+        return button
+        case "2" :
+            return button
+            case "3" :
+                return button
+                default :
+                return button
+}
